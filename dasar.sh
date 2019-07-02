3a4d0b6433cc523e71c8c95d67eff600ed5c2f15
Cyber404Exploit <52429107+Cyber404Exploit@users.noreply.github.com>
2019-07-02 17:05:38 +0700

    dasar.sh
    Command Line Dasar

---
+++ dasar.sh
@@ -0,0 +1,68 @@
+clear
+pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pkg install python-dev
+pkg install python2-dev
+pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pkg install python-dev
+pkg install python2-dev
+pkg install git
+pkg install curl
+pkg install php
+pip install lolcat
+pip2 install lolcat
+pkg install lolcat
+pkg install toilet
+pkg install figlet
+pip install request
+pip2 install request
+pkg install request
+pkg install nano
+pkg install bs4
+pkg install gem
+gem install lolcat
+pkg install mechanize
+pip install mechanize
+pip2 install mechanize
+apt update && apt upgrade
+apt update -y
+apt upgrade -y
+apt install php
+apt install php -y
+pkg install clang
+pkg install ruby
+pkg install gnupg
+pkg install wget
+pkg install neofetch
+pkg install screenfetch
+pkg install curl
+pkg install php
+pip install lolcat
+pip2 install lolcat
+pkg install lolcat
+pkg install toilet
+pkg install figlet
+pip install request
+pip2 install request
+pkg install request
+pkg install nano
+pkg install bs4
+pkg install gem
+gem install lolcat
+pkg install mechanize
+pip install mechanize
+pip2 install mechanize
+apt update && apt upgrade
+apt update -y
+apt upgrade -y
+apt install php
+apt install php -y
+pkg install clang
+pkg install ruby
+pkg install gnupg
+pkg install wget
+pkg install neofetch
+pkg install screenfetch
