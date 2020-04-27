74f2440e9160bbead2e20952409b417118bf6f08
syst3m_h4ck3d!!! <52429107+Cyber404Exploit@users.noreply.github.com>
2020-04-27 11:19:54 +0700

    Update dasar.sh

--- dasar.sh
+++ dasar.sh
@@ -2,67 +2,29 @@ clear
 pkg update && pkg upgrade
 pkg install python
 pkg install python2
-pkg install python-dev
-pkg install python2-dev
-pkg update && pkg upgrade
-pkg install python
-pkg install python2
-pkg install python-dev
-pkg install python2-dev
+pkg install python3
+pip install --upgrade pip
+pip2 install--upgrade pip
 pkg install git
 pkg install curl
 pkg install php
-pip install lolcat
-pip2 install lolcat
-pkg install lolcat
 pkg install toilet
 pkg install figlet
 pip install request
 pip2 install request
-pkg install request
 pkg install nano
-pkg install bs4
-pkg install gem
 gem install lolcat
-pkg install mechanize
 pip install mechanize
 pip2 install mechanize
-apt update && apt upgrade
-apt update -y
-apt upgrade -y
-apt install php
-apt install php -y
-pkg install clang
-pkg install ruby
-pkg install gnupg
 pkg install wget
 pkg install neofetch
 pkg install screenfetch
-pkg install curl
-pkg install php
-pip install lolcat
-pip2 install lolcat
-pkg install lolcat
-pkg install toilet
-pkg install figlet
-pip install request
-pip2 install request
-pkg install request
-pkg install nano
-pkg install bs4
-pkg install gem
-gem install lolcat
-pkg install mechanize
-pip install mechanize
-pip2 install mechanize
-apt update && apt upgrade
-apt update -y
-apt upgrade -y
-apt install php
-apt install php -y
-pkg install clang
-pkg install ruby
-pkg install gnupg
-pkg install wget
-pkg install neofetch
-pkg install screenfetch
+pip install emojify
+pip2 install strint
+pip install yagmail
+pip2 install yagmail
+pip install dst_enc
+pip2 install dst_enc
+pip install underscore
+pip install varAnimate
+pip2 install varAnimate
